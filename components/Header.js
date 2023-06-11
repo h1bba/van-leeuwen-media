d62b2be8d7be3f21b45259ad325511cd78bbed07
Ufuk <hibbayt@gmail.com>
2023-06-11 17:12:35 +0200

    NextJS boilertemplate & tailwind css

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+    return (
+        <Header>
+            <div className="bg-white">
+                <div></div>
+                <div>
+                    
+                </div>
+            </div>
+
+
+
+
+        </Header>
+    )
+
+
+}
+
+export default Header
