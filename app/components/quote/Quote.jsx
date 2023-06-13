33f4457cec43727becc3a000fd0159b315fc5067
Ufuk <hibbayt@gmail.com>
2023-06-13 03:24:49 +0200

    added quote component

---
+++ app/components/quote/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Quote.module.css'
+
+const Quote = () => {
+    return (
+        <div className={styles.quotecontainer}>
+            <p className='herotxt'>Niet je standaard studio, wel de <span className='color-orange'>juiste</span>.</p>
+        </div>
+    )
+}
+
+export default Quote
--- app/page.js
+++ app/page.js
@@ -5,41 +5,48 @@ import styles from "./page.module.css";
 import SelectedWork from './components/work/SelectedWork'
 import { BsFillPaletteFill } from 'react-icons/bs'
 import { BsFillCameraVideoFill } from 'react-icons/bs'
-import { BsSpeakerFill} from 'react-icons/bs'
+import { BsSpeakerFill } from 'react-icons/bs'
 import { BsSunriseFill } from 'react-icons/bs'
+import Quote from './components/quote/Quote'
 
 
 
 const Home = () => {
   return (
     <>
-      <h2 className={styles.herotxt}><span className="color-orange-bold">“</span>Elevate your brand with our <span className="color-orange">creative</span> video solutions<span className="color-orange-bold">”</span></h2>
+      <h2 className="herotxt"><span className="color-orange-bold">“</span>Elevate your brand with our <span className="color-orange">creative</span> video solutions<span className="color-orange-bold">”</span></h2>
       <div className="grid-container">
         <div className={styles.grid1}>
           <div className="ph br"></div>
         </div>
       </div>
       <div className="services-container">
-      <h2 className={styles.herotxt}><span className="color-orange-bold">“</span>Elevate your brand with our <span className="color-orange">creative</span> video solutions<span className="color-orange-bold">”</span></h2>
-      <div className="services-flex">
+        <h2 className="herotxt"><span className="color-orange-bold">“</span>Elevate your brand with our <span className="color-orange">creative</span> video solutions<span className="color-orange-bold">”</span></h2>
+        <div className="services-flex">
           <div className="services-badge br">
             <BsFillCameraVideoFill /><h2 className="service-title">Film/video</h2>
-            <p className="service-desc">Lorem Ipsum</p>
+            <p className="service-desc">Lorem ipsum dolor sit amet consectetur. Varius ridiculus porta scelerisque metus ipsum arcu orci. Magna dolor quam malesuada ac nunc pharetra et elementum. Mattis et convallis pellentesque elementum vel. Blandit pretium eget ut sed amet euismod volutpat.</p>
           </div>
 
           <div className="services-badge br">
             <BsSunriseFill /><h2 className="service-title">Motion design</h2>
+            <p className="service-desc">Lorem ipsum dolor sit amet consectetur. Varius ridiculus porta scelerisque metus ipsum arcu orci. Magna dolor quam malesuada ac nunc pharetra et elementum. Mattis et convallis pellentesque elementum vel. Blandit pretium eget ut sed amet euismod volutpat.</p>
           </div>
 
           <div className="services-badge br">
-          <BsFillPaletteFill /><h2 className="service-title">Color Grading</h2>
+            <BsFillPaletteFill /><h2 className="service-title">Color Grading</h2>
+            <p className="service-desc">Lorem ipsum dolor sit amet consectetur. Varius ridiculus porta scelerisque metus ipsum arcu orci. Magna dolor quam malesuada ac nunc pharetra et elementum. Mattis et convallis pellentesque elementum vel. Blandit pretium eget ut sed amet euismod volutpat.</p>
+
           </div>
 
           <div className="services-badge br">
             <BsSpeakerFill /><h2 className="service-title">Sound design</h2>
+            <p className="service-desc">Lorem ipsum dolor sit amet consectetur. Varius ridiculus porta scelerisque metus ipsum arcu orci. Magna dolor quam malesuada ac nunc pharetra et elementum. Mattis et convallis pellentesque elementum vel. Blandit pretium eget ut sed amet euismod volutpat.</p>
           </div>
+        </div>
       </div>
-      </div>
+      <Quote />
+
 
     </>
 
