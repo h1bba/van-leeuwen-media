ff9599e7eebbfce44537abe88bf107814e200b42
Ufuk <hibbayt@gmail.com>
2023-06-21 21:31:30 +0200

    progress framer motion

--- app/components/work/SelectedWork.jsx
+++ app/components/work/SelectedWork.jsx
@@ -1,36 +1,50 @@
 'use client'
 import React, { useEffect, useRef } from 'react';
 import styles from './SelectedWork.module.css';
-import { cubicBezier, EaseIn, useTransform, motion, useViewportScroll } from 'framer-motion';
+import { cubicBezier, EaseIn, useTransform, motion, useViewportScroll, easeIn, animate } from 'framer-motion';
 
 const SelectedWork = () => {
     const targetRef = useRef(null);
     const { scrollYProgress } = useViewportScroll();
 
-    const width = useTransform(scrollYProgress, [0, 0.2], ['99.6vw', '65vw']);
-    const borderRadius = useTransform(scrollYProgress, [0, 0.1, 0.2, 0.3], ['0px', '5px', '10px', '16px']);
+    const width = useTransform(scrollYProgress, [0, 0.2], ['100vw', '78em']);
+    const borderRadius = useTransform(scrollYProgress, [0, 0.3], ['0px', '32px']);
     const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.8])
-    const opacity = useTransform(scrollYProgress, [0, 0.1], [1, 0])
-    const position = useTransform(scrollYProgress, (pos) => {
-        return pos === 1 ? "fixed" : "relative";
-    })
+    const opacity = useTransform(scrollYProgress, [0, 0.15, 0.21], [1, 1, 0])
+    const translateY = useTransform(scrollYProgress, [0, 0.1], ['100%', '0%'])
+
+    const fadeUp = {
+        initial: {
+            opacity: 1
+        },
+        animate: {
+            opacity: 0
+        }
+    }
+
+
+
 
     return (
         <>
-            <motion.h2 style={{ opacity }} className="herotxt">
+            <motion.h2 style={{ opacity, marginTop: '6em' }} className="herotxt">
                 <span className="color-orange-bold">“</span>Elevate your brand with our{' '}
                 <span className="color-orange">creative</span> video solutions
                 <span className="color-orange-bold">”</span>
             </motion.h2>
 
-            <motion.div
-                style={{ position, width, borderRadius }}
-                ref={targetRef}
-                className={styles.fullwidthimg}
-            ></motion.div>
+            <div>
+                <motion.div variants={fadeUp}
+                    style={{ width, borderRadius }}
+                    ref={targetRef}
+                    className={styles.fullwidthimg}
+                ></motion.div>
+            </div>
             <div className={styles.sworkcontainer}>
                 <div className={styles.leftcolumn}>
                     <div className={styles.phitem1}></div>
+                    <div className={styles.phitem2}></div>
+
                 </div>
                 <div className={styles.rightcolumn}></div>
             </div>
