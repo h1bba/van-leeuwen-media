3054ae97d521be2d5c2f17df3050dd12a535a8c3
Ufuk <hibbayt@gmail.com>
2023-07-04 19:55:08 +0200

    added new font, still not loading

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import './globals.css'
 import Navbar from './components/navbar/Navbar.jsx'
 import Footer from './components/footer/Footer.jsx'
+import { Mulish } from 'next/font/google'
 
-
+const mulish = Mulish({ subsets: ['latin']})
 
 export const metadata = {
   title: 'Van Leeuwen Media',
--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import React from "react";
 import SelectedWork from './components/work/SelectedWork'
 import Ctabanner from './components/ctabanner/Ctabanner'
+import { Mulish } from 'next/font/google'
+const mulish = Mulish({ subsets: ['latin'], weight: ['400', '700'] })
+
+
 
 const Home = () => {
 
@@ -8,8 +12,10 @@ const Home = () => {
 
   return (
     <>
+    <main className="mulish.className">
       <SelectedWork />
       <Ctabanner />
+      </main>
     </>
 
   );
