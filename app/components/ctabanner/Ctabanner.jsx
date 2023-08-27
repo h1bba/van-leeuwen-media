65f90bc89bbce9120704a11d250d6827be813684
Ufuk <hibbayt@gmail.com>
2023-08-27 20:23:10 +0200

    added window code

--- app/components/ctabanner/Ctabanner.jsx
+++ app/components/ctabanner/Ctabanner.jsx
@@ -10,7 +10,6 @@ const Ctabanner = () => {
         offset: ['start end', 'end start'],
     });
 
-    // const opacity = useTransform(scrollYProgress, [0.4, 0.55], [1, 0]);
     const scale = useTransform(scrollYProgress, [0, 0.5], [1, 16]);
     const translateY = useTransform(scrollYProgress, [0, 1], [0, 16]);
     const borderRadius = useTransform(scrollYProgress, [0, 0.4], ['0px', '3em']);
@@ -18,7 +17,7 @@ const Ctabanner = () => {
     const backgroundColor = useTransform(scrollYProgress, [0, 1], ['black', 'white']);
 
     // Check if the viewport width is less than a certain value (e.g., 768 for mobile devices)
-    const isMobile = window.innerWidth < 768;
+    const isMobile = typeof window !== 'undefined' && window.innerWidth < 768;
 
     return (
         <>
@@ -30,7 +29,6 @@ const Ctabanner = () => {
                         </motion.p>
                     </motion.div>
                 </a>
-
             )}
         </>
     );
