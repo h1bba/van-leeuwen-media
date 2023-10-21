265fec0bc9c9b6a478bf29693024b3a16b107f8e
Ufuk <hibbayt@gmail.com>
2023-10-21 16:08:29 +0200

    autoplay

--- app/components/showreel/Showreel.jsx
+++ app/components/showreel/Showreel.jsx
@@ -48,7 +48,7 @@ export const Showreel = () => {
                     ref={targetRef}
 
                     className={styles.showreelcontainer}>
-                    <video autoplay="autoplay" loop="true" muted playsinline >
+                    <video autoPlay loop muted playsinline >
                         <source src={showreelmp4} type="video/mp4" />
                         <source src={showreelwebm} type="video/webm" />
                     </video>
